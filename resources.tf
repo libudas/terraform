e2230234ba95ade75da705741081c983a0179f82
Libudas <libudas@gmail.com>
2018-12-17 14:08:43 +0000

    azure network and subnet

--- resources.tf
+++ resources.tf
@@ -12,3 +12,21 @@ resource "aws_subnet" "subnet1" {
   vpc_id = "${aws_vpc.environement-example-two.id}"
   availability_zone = "eu-west-2a"
 }
+
+resource "azurerm_resource_group" "azy_network" {
+  location = "East US"
+  name= "devResourceGrp"
+}
+
+resource "azurerm_virtual_network" "blue_virtual_network" {
+  address_space =["10.0.0.0/16"]
+  location = "East US"
+  name = "bluevirtnetwork"
+  resource_group_name = "${azurerm_resource_group.azy_network.name}"
+  dns_servers = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+}
